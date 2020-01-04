bc17f3d9edef53faedd7d83a2086bdb18675aafb
testteam-hash <58592076+testteam-hash@users.noreply.github.com>
2020-01-04 18:17:46 +0530

    file uploaded

---
+++ Waiting.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Learning2018
+{
+    [TestClass]
+    public class Waiting
+    {
+        IWebDriver driver;
+        [TestMethod]
+        public void TestMethodforImplicitWait()
+        {
+            
+            try
+            {
+                driver = new ChromeDriver();
+                driver.Navigate().GoToUrl("https://www.google.co.in/");
+                driver.Manage().Window.Maximize();
+                //driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5); 
+                string HomeText = driver.FindElement(By.Id("menu-item-1635980")).Text;
+                
+
+            }
+            catch (Exception e)
+            {
+                Assert.Fail("Test Case failed, Could not find Home Button. ::: "+e.Message);
+                
+
+            }
+            finally
+            {
+                driver.Quit();
+            }
+
+        }
+    }
+}
